779e6a661782a87fe5d44a6967a23b230fc82f9d
AnshYadav2412 <2412anshyadav@gmail.com>
2026-02-01 03:59:14 +0530

    Project setup and React Routes setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Router,
+} from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Home from "./pages/dashboard/Home";
+import Signup from "./pages/auth/Signup";
+import Income from "./pages/dashboard/Income";
+import Expense from "./pages/dashboard/Expense";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
+
+const Root = () => {
+  //Check if token exists in Browser
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if Authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to={"/dashboard"} />
+  ) : (
+    <Navigate to={"/login"} />
+  );
+};
