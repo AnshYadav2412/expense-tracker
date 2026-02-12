f7f4659c22ff88575eeab311bc93f803cffd1c77
AnshYadav2412 <2412anshyadav@gmail.com>
2026-02-13 01:14:20 +0530

    Fixed negative value issue

--- frontend/src/components/dashboard/FinanceOverview.jsx
+++ frontend/src/components/dashboard/FinanceOverview.jsx
@@ -5,7 +5,6 @@ const COLORS = ["#875CF5", "#FA2C37", "#FF6900"];
 
 const FinanceOverview = ({ totalBalance, totalIncome, totalExpense }) => {
   const balanceData = [
-    { name: "Total Balance", amount: totalBalance },
     { name: "Total Income", amount: totalIncome },
     { name: "Total Expense", amount: totalExpense },
   ];
